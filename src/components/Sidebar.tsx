df0b92a1e911c66843d0b3ca3daafdef7334b6ce
Ivan Ray Altomera <irayspacii@gmail.com>
2021-08-10 16:05:21 +0800

    feat: sidebar source code

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Text,
   Heading,
   Pane,
   Link,
@@ -7,6 +8,7 @@ import {
   ControlIcon,
   SettingsIcon,
 } from "evergreen-ui";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 export default function Sidebar() {
   return (
@@ -58,6 +60,54 @@ export default function Sidebar() {
             </Link>
           </Pane>
         </Pane>
+      </Pane>
+      <Pane marginTop={15}>
+        <Text>Source code</Text>
+        <SyntaxHighlighter language="jsx">{`<Pane width={270} height={768} backgroundColor="#E6E8F0" marginTop={15}>
+    <Pane padding={30} textAlign="center">
+        <Heading backgroundColor="white" padding={30} color="#85A3FF">
+            Sidebar
+        </Heading>
+    </Pane>
+    <Pane>
+        <Pane>
+        <Link
+            paddingX={30}
+            paddingY={15}
+            display="grid"
+            gridTemplateColumns="26px auto"
+            href="#"
+            color="neutral"
+        >
+            <ApplicationIcon /> <span>Dashboard</span>
+        </Link>
+        </Pane>
+        <Pane>
+        <Link
+            paddingX={30}
+            paddingY={15}
+            display="grid"
+            gridTemplateColumns="26px auto"
+            href="#"
+            color="neutral"
+        >
+            <ControlIcon /> <span>Manage</span>
+        </Link>
+        </Pane>
+        <Pane>
+        <Link
+            paddingX={30}
+            paddingY={15}
+            display="grid"
+            gridTemplateColumns="26px auto"
+            href="#"
+            color="neutral"
+        >
+            <SettingsIcon /> <span>Settings</span>
+        </Link>
+        </Pane>
+    </Pane>
+</Pane>`}</SyntaxHighlighter>
       </Pane>
     </>
   );
